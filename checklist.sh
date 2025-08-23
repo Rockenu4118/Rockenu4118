57348a354d09dd0aef9d781a7db31246f1403920
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2025-08-22 21:07:56 -0700

    Update checklist.sh

--- checklist.sh
+++ checklist.sh
@@ -6,3 +6,19 @@ sudo apt install nvidia-driver
 
 # dev
 curl -f https://zed.dev/install.sh | sh
+sudo apt install terminator
+sudo apt install guake
+
+# kernel dev
+sudo apt update && sudo apt install -y \
+  build-essential \
+  binutils \
+  gcc \
+  g++ \
+  make \
+  qemu-system-i386 \
+  grub-pc-bin \
+  grub-common \
+  xorriso \
+  nasm
+
