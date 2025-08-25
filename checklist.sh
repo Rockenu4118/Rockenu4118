2a9b924274324d67a0fe49b84f0c130e9320ba99
AJ Hermosillo <61126875+Rockenu4118@users.noreply.github.com>
2025-08-24 17:54:01 -0700

    Update checklist.sh

--- checklist.sh
+++ checklist.sh
@@ -4,6 +4,9 @@ sudo apt update
 #drivers
 sudo apt install nvidia-driver
 
+#general
+sudo apt install flatpak
+
 # dev
 curl -f https://zed.dev/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
